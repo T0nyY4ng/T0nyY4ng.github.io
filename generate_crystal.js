81e7aca9ffc75f53d53dbbb46d49a4bf16a0dbf4
羽 <qzhhj0917@163.com>
2026-08-18 18:55:10 +0800

    Crystal: pure color without textures

--- generate_crystal.js
+++ generate_crystal.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
 function buildEndCrystalModel() {
   const positions = [];
   const normals = [];
-  const uvs = [];
   const indices = [];
   const thicknesses = [];
   const aos = [];
@@ -11,14 +11,13 @@ function buildEndCrystalModel() {
   const daoPs = [];
   const SNs = [];
 
-  function addQuad(p0, p1, p2, p3, n, uv0, uv1, uv2, uv3, thick, aoVal) {
+  function addQuad(p0, p1, p2, p3, n, thick, aoVal) {
     if (thick === undefined) thick = 0.5;
     if (aoVal === undefined) aoVal = 1.0;
     const baseIdx = positions.length / 3;
     positions.push(...p0, ...p1, ...p2, ...p3);
     normals.push(...n, ...n, ...n, ...n);
     SNs.push(...n, ...n, ...n, ...n);
-    uvs.push(...uv0, ...uv1, ...uv2, ...uv3);
     thicknesses.push(thick, thick, thick, thick);
     aos.push(aoVal, aoVal, aoVal, aoVal);
     daoNs.push(...n, ...n, ...n, ...n);
@@ -28,11 +27,7 @@ function buildEndCrystalModel() {
     indices.push(baseIdx, baseIdx + 2, baseIdx + 3);
   }
 
-  function addBox(minX, minY, minZ, maxX, maxY, maxZ, rotMatrix, thick, ao, uvRect) {
-    const u0 = uvRect ? uvRect[0] : 0.0;
-    const v0 = uvRect ? uvRect[1] : 0.0;
-    const u1 = uvRect ? uvRect[2] : 1.0;
-    const v1 = uvRect ? uvRect[3] : 1.0;
+  function addBox(minX, minY, minZ, maxX, maxY, maxZ, rotMatrix, thick, ao) {
     if (thick === undefined) thick = 0.3;
     if (ao === undefined) ao = 1.0;
     const transform = (p) => {
@@ -54,37 +49,37 @@ function buildEndCrystalModel() {
     addQuad(
       transform([minX, minY, maxZ]), transform([maxX, minY, maxZ]),
       transform([maxX, maxY, maxZ]), transform([minX, maxY, maxZ]),
-      transformNorm([0, 0, 1]), [u0, v0], [u1, v0], [u1, v1], [u0, v1], thick, ao
+      transformNorm([0, 0, 1]), thick, ao
     );
     // -Z (back)
     addQuad(
       transform([maxX, minY, minZ]), transform([minX, minY, minZ]),
       transform([minX, maxY, minZ]), transform([maxX, maxY, minZ]),
-      transformNorm([0, 0, -1]), [u0, v0], [u1, v0], [u1, v1], [u0, v1], thick, ao
+      transformNorm([0, 0, -1]), thick, ao
     );
     // +Y (top)
     addQuad(
       transform([minX, maxY, maxZ]), transform([maxX, maxY, maxZ]),
       transform([maxX, maxY, minZ]), transform([minX, maxY, minZ]),
-      transformNorm([0, 1, 0]), [u0, v0], [u1, v0], [u1, v1], [u0, v1], thick, ao
+      transformNorm([0, 1, 0]), thick, ao
     );
     // -Y (bottom)
     addQuad(
       transform([minX, minY, minZ]), transform([maxX, minY, minZ]),
       transform([maxX, minY, maxZ]), transform([minX, minY, maxZ]),
-      transformNorm([0, -1, 0]), [u0, v0], [u1, v0], [u1, v1], [u0, v1], thick, ao
+      transformNorm([0, -1, 0]), thick, ao
     );
     // +X (right)
     addQuad(
       transform([maxX, minY, maxZ]), transform([maxX, minY, minZ]),
       transform([maxX, maxY, minZ]), transform([maxX, maxY, maxZ]),
-      transformNorm([1, 0, 0]), [u0, v0], [u1, v0], [u1, v1], [u0, v1], thick, ao
+      transformNorm([1, 0, 0]), thick, ao
     );
     // -X (left)
     addQuad(
       transform([minX, minY, minZ]), transform([minX, minY, maxZ]),
       transform([minX, maxY, maxZ]), transform([minX, maxY, minZ]),
-      transformNorm([-1, 0, 0]), [u0, v0], [u1, v0], [u1, v1], [u0, v1], thick, ao
+      transformNorm([-1, 0, 0]), thick, ao
     );
   }
 
@@ -113,22 +108,21 @@ function buildEndCrystalModel() {
   function buildNotchedCubeFrame(size, beamW, rotMat, thick, ao) {
     const s = size / 2;
     const b = beamW;
-    const glassUV = [32/128, 0/64, 64/128, 32/64];
     // 12 outer beams
-    addBox(-s, -s, -s, -s+b, -s+b, s, rotMat, thick, ao, glassUV);
-    addBox(s-b, -s, -s, s, -s+b, s, rotMat, thick, ao, glassUV);
-    addBox(-s, s-b, -s, -s+b, s, s, rotMat, thick, ao, glassUV);
-    addBox(s-b, s-b, -s, s, s, s, rotMat, thick, ao, glassUV);
+    addBox(-s, -s, -s, -s+b, -s+b, s, rotMat, thick, ao);
+    addBox(s-b, -s, -s, s, -s+b, s, rotMat, thick, ao);
+    addBox(-s, s-b, -s, -s+b, s, s, rotMat, thick, ao);
+    addBox(s-b, s-b, -s, s, s, s, rotMat, thick, ao);
 
-    addBox(-s, -s+b, -s, -s+b, s-b, -s+b, rotMat, thick, ao, glassUV);
-    addBox(s-b, -s+b, -s, s, s-b, -s+b, rotMat, thick, ao, glassUV);
-    addBox(-s, -s+b, s-b, -s+b, s-b, s, rotMat, thick, ao, glassUV);
-    addBox(s-b, -s+b, s-b, s, s-b, s, rotMat, thick, ao, glassUV);
+    addBox(-s, -s+b, -s, -s+b, s-b, -s+b, rotMat, thick, ao);
+    addBox(s-b, -s+b, -s, s, s-b, -s+b, rotMat, thick, ao);
+    addBox(-s, -s+b, s-b, -s+b, s-b, s, rotMat, thick, ao);
+    addBox(s-b, -s+b, s-b, s, s-b, s, rotMat, thick, ao);
 
-    addBox(-s+b, -s, -s, s-b, -s+b, -s+b, rotMat, thick, ao, glassUV);
-    addBox(-s+b, s-b, -s, s-b, s, -s+b, rotMat, thick, ao, glassUV);
-    addBox(-s+b, -s, s-b, s-b, -s+b, s, rotMat, thick, ao, glassUV);
-    addBox(-s+b, s-b, s-b, s-b, s, s, rotMat, thick, ao, glassUV);
+    addBox(-s+b, -s, -s, s-b, -s+b, -s+b, rotMat, thick, ao);
+    addBox(-s+b, s-b, -s, s-b, s, -s+b, rotMat, thick, ao);
+    addBox(-s+b, -s, s-b, s-b, -s+b, s, rotMat, thick, ao);
+    addBox(-s+b, s-b, s-b, s-b, s, s, rotMat, thick, ao);
 
     // Minecraft end crystal tooth pattern on all faces (notched pixel pattern)
     const toothSize = b * 0.9;
@@ -138,21 +132,21 @@ function buildEndCrystalModel() {
       if (i % 2 === 1) {
         const offset = -s + b + (i / (count + 1)) * (size - 2*b);
         // Face +/- Z
-        addBox(offset - toothSize/2, s - b - toothDepth, s - b, offset + toothSize/2, s - b, s, rotMat, thick, ao, glassUV);
-        addBox(offset - toothSize/2, -s + b, s - b, offset + toothSize/2, -s + b + toothDepth, s, rotMat, thick, ao, glassUV);
-        addBox(-s, offset - toothSize/2, s - b, -s + b + toothDepth, offset + toothSize/2, s, rotMat, thick, ao, glassUV);
-        addBox(s - b - toothDepth, offset - toothSize/2, s - b, s, offset + toothSize/2, s, rotMat, thick, ao, glassUV);
+        addBox(offset - toothSize/2, s - b - toothDepth, s - b, offset + toothSize/2, s - b, s, rotMat, thick, ao);
+        addBox(offset - toothSize/2, -s + b, s - b, offset + toothSize/2, -s + b + toothDepth, s, rotMat, thick, ao);
+        addBox(-s, offset - toothSize/2, s - b, -s + b + toothDepth, offset + toothSize/2, s, rotMat, thick, ao);
+        addBox(s - b - toothDepth, offset - toothSize/2, s - b, s, offset + toothSize/2, s, rotMat, thick, ao);
 
-        addBox(offset - toothSize/2, s - b - toothDepth, -s, offset + toothSize/2, s - b, -s + b, rotMat, thick, ao, glassUV);
-        addBox(offset - toothSize/2, -s + b, -s, offset + toothSize/2, -s + b + toothDepth, -s + b, rotMat, thick, ao, glassUV);
-        addBox(-s, offset - toothSize/2, -s, -s + b + toothDepth, offset + toothSize/2, -s + b, rotMat, thick, ao, glassUV);
-        addBox(s - b - toothDepth, offset - toothSize/2, -s, s, offset + toothSize/2, -s + b, rotMat, thick, ao, glassUV);
+        addBox(offset - toothSize/2, s - b - toothDepth, -s, offset + toothSize/2, s - b, -s + b, rotMat, thick, ao);
+        addBox(offset - toothSize/2, -s + b, -s, offset + toothSize/2, -s + b + toothDepth, -s + b, rotMat, thick, ao);
+        addBox(-s, offset - toothSize/2, -s, -s + b + toothDepth, offset + toothSize/2, -s + b, rotMat, thick, ao);
+        addBox(s - b - toothDepth, offset - toothSize/2, -s, s, offset + toothSize/2, -s + b, rotMat, thick, ao);
 
         // Face +/- X
-        addBox(s - b, offset - toothSize/2, s - b - toothDepth, s, offset + toothSize/2, s - b, rotMat, thick, ao, glassUV);
-        addBox(s - b, offset - toothSize/2, -s + b, s, offset + toothSize/2, -s + b + toothDepth, rotMat, thick, ao, glassUV);
-        addBox(-s, offset - toothSize/2, s - b - toothDepth, -s + b, offset + toothSize/2, s - b, rotMat, thick, ao, glassUV);
-        addBox(-s, offset - toothSize/2, -s + b, -s + b, offset + toothSize/2, -s + b + toothDepth, rotMat, thick, ao, glassUV);
+        addBox(s - b, offset - toothSize/2, s - b - toothDepth, s, offset + toothSize/2, s - b, rotMat, thick, ao);
+        addBox(s - b, offset - toothSize/2, -s + b, s, offset + toothSize/2, -s + b + toothDepth, rotMat, thick, ao);
+        addBox(-s, offset - toothSize/2, s - b - toothDepth, -s + b, offset + toothSize/2, s - b, rotMat, thick, ao);
+        addBox(-s, offset - toothSize/2, -s + b, -s + b, offset + toothSize/2, -s + b + toothDepth, rotMat, thick, ao);
       }
     }
   }
@@ -174,13 +168,11 @@ function buildEndCrystalModel() {
   // 4. Central Glowing Crystal Core (Single solid geometric crystal core, precisely scaled and rotated without protrusion)
   const coreSize = 0.52;
   const rotCore = multMatrix(getRotationMatrix([1, 0, 1].map(v => v/Math.SQRT2), Math.PI/4), getRotationMatrix([0, 1, 0], Math.PI/4));
-  const coreUV = [0/128, 0/64, 32/128, 32/64];
-  addBox(-coreSize/2, -coreSize/2, -coreSize/2, coreSize/2, coreSize/2, coreSize/2, rotCore, 0.95, 1.0, coreUV);
+  addBox(-coreSize/2, -coreSize/2, -coreSize/2, coreSize/2, coreSize/2, coreSize/2, rotCore, 0.95, 1.0);
 
   // 5. Inner Core Concentric Rune Beacon (Sharing same rotation axis to prevent any corner sticking out)
   const nucSize = 0.36;
-  const nucUV = [0/128, 0/64, 16/128, 16/64];
-  addBox(-nucSize/2, -nucSize/2, -nucSize/2, nucSize/2, nucSize/2, nucSize/2, rotCore, 1.0, 1.0, nucUV);
+  addBox(-nucSize/2, -nucSize/2, -nucSize/2, nucSize/2, nucSize/2, nucSize/2, rotCore, 1.0, 1.0);
 
   // Package into .buf format
   const vCount = positions.length / 3;
@@ -190,61 +182,59 @@ function buildEndCrystalModel() {
     vertexCount: vCount,
     indexCount: iCount,
     attributes: [
-      { id: 'daoN', needsPack: false, componentSize: 3, storageType: 'Float32Array' },
-      { id: 'normal', needsPack: false, componentSize: 3, storageType: 'Float32Array' },
-      { id: 'uv', needsPack: false, componentSize: 2, storageType: 'Float32Array' },
+      { id: "daoN", needsPack: false, componentSize: 3, storageType: "Float32Array" },
+      { id: "normal", needsPack: false, componentSize: 3, storageType: "Float32Array" },
       {
-        id: 'SN',
+        id: "SN",
         needsPack: true,
         componentSize: 3,
-        storageType: 'Int16Array',
+        storageType: "Int16Array",
         packedComponents: [{ from: -1, delta: 2 }, { from: -1, delta: 2 }, { from: -1, delta: 2 }]
       },
       {
-        id: 'ao',
+        id: "ao",
         needsPack: true,
         componentSize: 1,
-        storageType: 'Uint16Array',
+        storageType: "Uint16Array",
         packedComponents: [{ from: 0, delta: 1 }]
       },
       {
-        id: 'daoP',
+        id: "daoP",
         needsPack: true,
         componentSize: 3,
-        storageType: 'Int16Array',
+        storageType: "Int16Array",
         packedComponents: [{ from: 0, delta: 1 }, { from: 0, delta: 1 }, { from: 0, delta: 1 }]
       },
-      { id: 'indices', needsPack: false, componentSize: 1, storageType: 'Uint16Array' },
+      { id: "indices", needsPack: false, componentSize: 1, storageType: "Uint16Array" },
       {
-        id: 'position',
+        id: "position",
         needsPack: true,
         componentSize: 3,
-        storageType: 'Uint16Array',
+        storageType: "Uint16Array",
         packedComponents: [{ from: -1, delta: 2 }, { from: -1, delta: 2 }, { from: -1, delta: 2 }]
       },
       {
-        id: 'thickness',
+        id: "thickness",
         needsPack: true,
         componentSize: 1,
-        storageType: 'Uint8Array',
+        storageType: "Uint8Array",
         packedComponents: [{ from: 0, delta: 1 }]
       }
     ],
-    meshType: 'Mesh'
+    meshType: "Mesh"
   };
 
   let headerJson = JSON.stringify(header);
-  let headerBuf = Buffer.from(headerJson, 'utf8');
+  let headerBuf = Buffer.from(headerJson, "utf8");
   while ((4 + headerBuf.length) % 4 !== 0) {
-    headerJson += ' ';
-    headerBuf = Buffer.from(headerJson, 'utf8');
+    headerJson += " ";
+    headerBuf = Buffer.from(headerJson, "utf8");
   }
   const headerLen = headerBuf.length;
 
   // Binary buffers
   const daoNArr = new Float32Array(daoNs);
   const normalArr = new Float32Array(normals);
-  const uvArr = new Float32Array(uvs);
   
   // SN: Int16Array
   const snArr = new Int16Array(vCount * 3);
@@ -287,7 +277,6 @@ function buildEndCrystalModel() {
     headerBuf,
     Buffer.from(daoNArr.buffer),
     Buffer.from(normalArr.buffer),
-    Buffer.from(uvArr.buffer),
     Buffer.from(snArr.buffer),
     Buffer.from(aoArr.buffer),
     Buffer.from(daoPArr.buffer),
@@ -298,9 +287,10 @@ function buildEndCrystalModel() {
   fileBuffers[0].writeUInt32LE(headerLen, 0);
 
   const finalBuf = Buffer.concat(fileBuffers);
-  fs.writeFileSync('D:/Client/AbyssTechnology-1.21.1-src/web/T0nyY4ng.github.io/assets/models/home/cross.buf', finalBuf);
-  fs.writeFileSync('D:/Client/AbyssTechnology-1.21.1-src/web/T0nyY4ng.github.io/assets/models/home/cross_ld.buf', finalBuf);
-  console.log('Successfully generated End Crystal binary model! Vertices:', vCount, 'Indices:', iCount, 'Bytes:', finalBuf.length);
+  const dir = path.join(__dirname, "assets/models/home");
+  fs.writeFileSync(path.join(dir, "cross.buf"), finalBuf);
+  fs.writeFileSync(path.join(dir, "cross_ld.buf"), finalBuf);
+  console.log("Successfully rebuilt pure color End Crystal binary model! Vertices:", vCount, "Indices:", iCount, "Bytes:", finalBuf.length);
 }
 
 buildEndCrystalModel();
